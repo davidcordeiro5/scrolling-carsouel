e72878960563b33e05a7fa6e1f14649c34b09c29
david_c <david.cordeiro@epitech.eu>
2022-12-12 11:13:19 +0100

    [feat] add translate3d /w preserve-3d
    to get the 3d vinyl's effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { DOMAttributes, MouseEventHandler, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const squares = ["#9499ff", "#daffef", "#149eca", "#dc8c8c", "#bebebe"];
+  const length = squares.length;
+
+  const arr = ["1", "2", "3", "4", "5", "6", "7"];
+
+  console.log("arr", arr.length);
+
+  const [displayedItem, setDisplayedItem] = useState<number[]>([0, 1, 2, 3]);
+
+  console.log("arr :>> ", arr);
+
+  const handle = (action: "add" | "pop") => {
+    return (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+      if (action === "add") {
+        setDisplayedItem((curr) =>
+          curr.map((elem) =>
+            elem === arr.length - 1 ? (elem = 0) : (elem += 1)
+          )
+        );
+      } else {
+        setDisplayedItem((curr) =>
+          curr.map((elem) =>
+            elem === 0 ? (elem = arr.length - 1) : (elem -= 1)
+          )
+        );
+      }
+    };
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="page">
+      {/* <h1 style={{ color: "black" }}>
+        {arr[displayedItem[0]]}, {arr[displayedItem[1]]},{" "}
+        {arr[displayedItem[2]]}, {arr[displayedItem[3]]}
+      </h1>
+      <button onClick={handle("add")}>++</button>
+      <button onClick={handle("pop")}>--</button> */}
+      {squares.map((s, i) => (
+        <div
+          style={{
+            backgroundColor: s,
+            // transform: `scale(${0.5 + i / 10})`,
+
+            //NOTE: Change translate3d Y according to the device
+            transform: `translate3d(0px, ${0 - i * 50}px, ${0 - i * 75}px)`,
+
+            zIndex: `${length - i}`,
+          }}
+          className="square"
+          key={i}
+        />
+      ))}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
